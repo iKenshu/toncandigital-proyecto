f7aa68943917d15d1231b49bf918649d5738a76d
ikenshu <undefined.hlo.o@gmail.com>
2016-08-03 08:32:24 -0400

    detallitos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
- $(document).on('click', 'a[href*="#"]:not([href="#"])', function(event){
+$(document).on('click', 'a[href*="#"]:not([href="#"])', function(event){
 	event.preventDefault();
 
 	$('html, body').animate({
-	    scrollTop: $( $.attr(this, 'href') ).offset().top - 50
+		scrollTop: $( $.attr(this, 'href') ).offset().top - 50
 	}, 700);
-
 }); 
 
 
